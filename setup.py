1788dd9151d63bf13f441fc7ebe2d4da84121c4f
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2023-03-10 17:39:31 +0000

    A few modifications to the setup file

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/simon-m-mudd/lsdviztools',
+    url='https://github.com/LSDTopotools/lsdviztools',
     version='0.4.10',
     zip_safe=False,
 )
